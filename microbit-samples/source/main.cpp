484b3640ba24e51979dbd38873cafc11677d44ef
quentinbastos <bastos.quentin.pro@gmail.com>
2026-03-24 10:01:53 +0100

    feat(last ex): boussole

--- microbit-samples/source/main.cpp
+++ microbit-samples/source/main.cpp
@@ -53,21 +53,37 @@ void onTiltLeft(MicroBitEvent e)
     uBit.display.scroll(tiltLeftCount);
 }
 
+void onNorthFacing(MicroBitEvent e)
+{
+    int heading = uBit.compass.heading();
+    // Le Nord est entre 315° et 45°
+    if (heading < 45 || heading > 315) {
+        uBit.display.print("N");
+    } else {
+        uBit.display.clear(); 
+    }
+}
+
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
 
     // Enregistrement des événements lors du clic sur les boutons A et B
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+    // uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+    // uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
 
 
     // Enregistrement d'un événement pour afficher la température toutes les 5 secondes
-    uBit.messageBus.listen(MICROBIT_ID_THERMOMETER, MICROBIT_THERMOMETER_EVT_UPDATE, temperature);
+    // uBit.messageBus.listen(MICROBIT_ID_THERMOMETER, MICROBIT_THERMOMETER_EVT_UPDATE, temperature);
 
     // Enregistrement d'un événement pour compter le nombre de fois que le micro:bit est incliné vers la gauche
-    uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_TILT_LEFT, onTiltLeft);
+    // uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_TILT_LEFT, onTiltLeft);
+
+    // Enregistrement d'un événement pour afficher le nord lorsque le micro:bit est vers le nord.
+    uBit.compass.calibrate();
+    uBit.messageBus.listen(MICROBIT_ID_COMPASS, MICROBIT_COMPASS_EVT_DATA_UPDATE, onNorthFacing);
+
     // Message d'accueil sur l'écran LED
     uBit.display.scroll("PRETS?");
 
